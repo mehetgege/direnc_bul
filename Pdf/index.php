8c6b5ca22b8a3a62d34988f3814b1c3d020b927a
mehetgege <mehetgege@gmail.com>
2025-07-15 21:35:19 +0300

    Delete Pdf directory

--- Pdf/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-$files = array_filter(scandir('./'), function($f) { 
-  return pathinfo($f, PATHINFO_EXTENSION) === 'pdf'; 
-});
-echo json_encode(array_values($files));
-?>
